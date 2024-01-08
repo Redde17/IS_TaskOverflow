398751beca82fc67c9f704de0b58428f9c163c4e
Redde17 <redsmile797@gmail.com>
2024-01-08 22:42:31 +0100

    Aggiunta prime icone per la navBar e inizio codifica della navBar. Creazione di un file apposito per la navBar e aggiunto il suo riferimento in HomeView.axaml. Codificata la struttura e la disposizione della navBar.

---
+++ TaskOverflow/Views/HomeViewElems/NavBar.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace TaskOverflow.Views.HomeViewElems
+{
+    public partial class NavBar : UserControl
+    {
+        public NavBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
